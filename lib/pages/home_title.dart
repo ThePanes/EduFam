fc4c8d569d67dab3274dc65c095b083cea13f8da
LordPanes <oscar.montecinos.guerrero@gmail.com>
2025-06-22 04:37:23 -0400

    implementacion de cambios de persistencia del perfil al modificarse

--- lib/pages/home_title.dart
+++ lib/pages/home_title.dart
@@ -1,4 +1,4 @@
-
+import 'package:aplication_laboratorio/data/user_db.dart';
 import 'package:aplication_laboratorio/pages/start_page.dart';
 import 'package:flutter/material.dart';
 
@@ -11,12 +11,30 @@ class HomeTitle extends StatefulWidget {
 }
 
 class _HomeTitleState extends State<HomeTitle> {
+  UserProfile? user;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadUser();
+  }
+
+  Future<void> _loadUser() async {
+    final loadedUser = await UserDB.getUser();
+    setState(() {
+      user = loadedUser;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    if (user == null) {
+      return const Center(child: CircularProgressIndicator());
+    }
     return Scaffold(
       appBar: AppBar(
-        clipBehavior: Clip.antiAlias,title:
-        Column(
+        clipBehavior: Clip.antiAlias,
+        title: Column(
           children: [
             Text("Edufam"),
           ],
@@ -24,23 +42,24 @@ class _HomeTitleState extends State<HomeTitle> {
       ),
       body: Center(
         child: Column(
-          
-          mainAxisAlignment: MainAxisAlignment.center, // Centra verticalmente
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Card(
               clipBehavior: Clip.antiAlias,
-              child: Column(mainAxisAlignment: MainAxisAlignment.end,children: [Image.asset('assets/edufam.png',fit: BoxFit.cover,width: 100,)],),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [Image.asset('assets/edufam.png', fit: BoxFit.cover, width: 100,)],
+              ),
             ),
-            
             const Icon(
               Icons.person,
               size: 80,
               color: Color.fromARGB(255, 24, 152, 215),
             ),
             const SizedBox(height: 16),
-            const Text(
-              'Bienvenido, "Usuario"',
-              style: TextStyle(fontSize: 20),
+            Text(
+              'Bienvenido, "${user?.nombre ?? "Usuario"}"',
+              style: const TextStyle(fontSize: 20),
             ),
             const SizedBox(height: 24),
             TextButton(
